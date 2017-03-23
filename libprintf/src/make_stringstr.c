92a7d850968c0910102108ae15d06e35051b339a
Indy <kdevlinmurray@gmail.com>
2017-03-22 21:24:57 -0700

    I think I'm ready to submit

--- libprintf/src/make_stringstr.c
+++ libprintf/src/make_stringstr.c
@@ -6,22 +6,17 @@
 /*   By: kmurray <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/20 21:10:33 by kmurray           #+#    #+#             */
-/*   Updated: 2017/03/22 21:23:34 by kmurray          ###   ########.fr       */
+/*   Updated: 2017/03/22 21:24:27 by kmurray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
-#include <stdio.h>
 
 char	*make_stringstr(t_mods *mods, va_list ap)
 {
 	char	*str;
 
-	if (mods->length == L){
-		printf("%ls", va_arg(ap, wchar_t *));
-		str = ft_strdup("doodoo");}
-	else
-		str = va_arg(ap, char *);
+	str = va_arg(ap, char *);
 	if (!str)
 		return (ft_strdup("(null)"));
 	str = ft_strdup(str);
