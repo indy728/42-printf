d0011926160fa8dab9f97d08a28b5543686eba34
Indy <kdevlinmurray@gmail.com>
2017-03-22 21:23:37 -0700

    temp for testing online

--- libprintf/src/make_stringstr.c
+++ libprintf/src/make_stringstr.c
@@ -6,7 +6,7 @@
 /*   By: kmurray <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/20 21:10:33 by kmurray           #+#    #+#             */
-/*   Updated: 2017/03/22 21:22:38 by kmurray          ###   ########.fr       */
+/*   Updated: 2017/03/22 21:23:34 by kmurray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*make_stringstr(t_mods *mods, va_list ap)
 
 	if (mods->length == L){
 		printf("%ls", va_arg(ap, wchar_t *));
-		return ("doodoo");}
+		str = ft_strdup("doodoo");}
 	else
 		str = va_arg(ap, char *);
 	if (!str)
